6af7d197d1c7abe508491e5cde3d2e18f1e9b856
NIco <industria_4.0@yahoo.com>
2020-12-02 10:55:57 -0500

    tsc --init
    archivo de configuracion inicila

---
+++ typescript/app.js
@@ -0,0 +1,7 @@
+function saludar(nombre) {
+    console.log('Hola' + nombre);
+}
+var wolverin = {
+    nombre: ' Logan1'
+};
+saludar(wolverin.nombre);
---
+++ typescript/app.ts
@@ -0,0 +1,9 @@
+function saludar(nombre: string) {
+  console.log('Hola' + nombre);
+}
+
+var wolverin = {
+  nombre: ' Logan',
+};
+
+saludar(wolverin.nombre);
