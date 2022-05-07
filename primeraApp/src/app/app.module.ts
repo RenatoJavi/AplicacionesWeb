435686aff259640f908c8599b95c35618024e649
NIco <rjavierse@hotmail.com>
2022-05-07 09:35:51 -0500

    crear componentes

--- primeraApp/src/app/app.module.ts
+++ primeraApp/src/app/app.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BodyComponente } from './componentes/body/body.componente';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponente,
+    
+    
   ],
   imports: [
     BrowserModule
--- primeraApp/src/app/componentes/body/body.componente.ts
+++ primeraApp/src/app/componentes/body/body.componente.ts
@@ -4,7 +4,7 @@ import {Component} from '@angular/core'
 @Component({
 
 selector:'app-body',
-templateUrl:'./body.component.html'
+templateUrl:'./body.componente.html'
 
 })
 
@@ -12,11 +12,14 @@ export class BodyComponente{
 
 personaje:any={
 
-        frase:'Un gran poder, una gran responsavilidad.',
+        frase:'Un gran poder, una gran responsabilidad.',
         autor:'Ben Parker',
+        frase2:'A luchar por la justicia',
+        autor2:'Superman'
 
 }
 mostrar=true;
 
 personajes:string[]=['spider','venon','octupos','Mery Jen','Tia May'];
+villanos:String[]=['octupus','dr frio','pinguino','duende verde','arena'];
 }
